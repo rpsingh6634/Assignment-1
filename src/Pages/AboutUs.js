c917aedbe81e8c16e71fa77ed8043ea9d2f31ee3
rpsingh6634 <rpsingh6634@gmail.com>
2022-11-19 11:54:30 +0530

    first commit

---
+++ src/Pages/AboutUs.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam, perferendis.</p>
+            <br/>
+            <p style={{margin: '15px'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto magnam amet a dolorum hic ducimus quis voluptas eaque nostrum culpa rem ut optio sequi fugit ad ipsa suscipit temporibus tenetur sunt, pariatur maiores nam! Tempora ex minima porro unde, quisquam modi iusto rerum delectus quasi expedita qui voluptas nulla nihil pariatur. Voluptate incidunt eligendi est ipsam quasi. Provident, esse quasi voluptatem explicabo placeat ipsum excepturi dolores! Dolor sint dolore debitis modi sapiente illo nisi. Iste quod consectetur perspiciatis. Repudiandae pariatur culpa obcaecati, velit odit possimus, unde commodi vitae vel cum neque explicabo repellendus quod rem.</p>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis facere eaque debitis. Unde libero, deleniti molestiae corporis praesentium excepturi nisi, ab consectetur repellat, rem obcaecati nobis accusantium sunt labore ea!_________.</p>
+        </div>
+    )
+}
+
+export default AboutUs
+
